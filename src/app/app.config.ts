a98f68b285cf7d6e6703f2f1ed31cdc268efd39e
Sarah Delfino <sarahdelfino7@gmail.com>
2025-11-26 10:40:11 -0500

    tweaks

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,14 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAnalytics, provideAnalytics, ScreenTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "madebycypress", appId: "1:570919053588:web:fe862b5c3a136885e97a01", storageBucket: "madebycypress.firebasestorage.app", apiKey: "AIzaSyBDulpUYaZPk_lJMlCAhJzXlanjrDYSI94", authDomain: "madebycypress.firebaseapp.com", messagingSenderId: "570919053588", measurementId: "G-N669RP65ZB" })), provideAnalytics(() => getAnalytics()), ScreenTrackingService, provideFirestore(() => getFirestore())
   ]
 };
